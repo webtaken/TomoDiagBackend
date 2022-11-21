551603fad44e5b906d45915b1ca2da96eee5f91a
saul <luckly083@gmail.com>
2022-11-21 16:37:31 -0500

    Fixing status is not a function

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(diagnosticoRoutes);
 
 // Global error handling
-app.use(function (err, _req, res) {
+app.use(function (err, _req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
